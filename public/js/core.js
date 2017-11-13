04a20fef1850a14c7647d529ee6b5e870865d5cd
Troy Williams <troywilliams@Troys-MacBook-Pro.local>
2017-11-13 12:08:57 -0600

    add statement

--- public/js/core.js
+++ public/js/core.js
@@ -5,5 +5,8 @@ let skills = document.getElementById('skills');
 skills.textContent = `Making things look pretty and writing quality code. Proficient with JavaScript, HTML, Bootstrap, and CSS. Intermediate AngularJS, Wordpress, and React Frameworks.
 Experience with REST apis, Express, MongoDB and SQL.`;
 
+let state = document.getElementById('state');
+state.textContent = 'A double threat with a sales and account management background with early startup experience and the skills to build web applications.'
+
 let invite = document.getElementById('invite');
 invite.textContent = 'Want to chat? I know a few good coffee shops!';
