17312b60753265e04cbe46cf49de22559c1fbb5a
Troy Williams <troywilliams@Troys-MacBook-Pro.local>
2017-11-13 11:50:18 -0600

    add skills, css rules, pdf for resume.

--- public/js/core.js
+++ public/js/core.js
@@ -1,3 +1,9 @@
 let intro = document.getElementById('intro');
-intro.textContent = "Troy Williams. Aspiring Web Developer. Experienced Sales Executive."
+intro.textContent = 'Troy Williams. Aspiring Web Developer. Experienced Sales Executive.';
 
+let skills = document.getElementById('skills');
+skills.textContent = `Making things look pretty and writing quality code. Proficient with JavaScript, HTML, Bootstrap, and CSS. Intermediate AngularJS, Wordpress, and React Frameworks.
+Experience with REST apis, Express, MongoDB and SQL.`;
+
+let invite = document.getElementById('invite');
+invite.textContent = 'Want to chat? I know a few good coffee shops!';
